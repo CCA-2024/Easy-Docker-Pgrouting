5dd8d5e96f55eeb8b6f0fe07e0c72bbad8416ce7
DevPyPedro <dev.santiago.felicio@gmail.com>
2025-01-15 11:51:17 -0300

    Update consumer.py

--- src/consumer.py
+++ src/consumer.py
@@ -3,17 +3,19 @@ import os
 
 class OsmComsumer:
     def __init__(self)->None:
-        self.volume_path = "./src/OSMs"
+        self.volume_path = "./OSMs"
         
     def run_file(self)-> str:
         '''Verifica se o diretorio é valido'''
-        
-        if os.path.exists(self.volume_path):
-            
-            for filename in os.listdir(self.volume_path):
-                filepath = os.path.join(self.volume_path, filename)
-                command = f"osm2pgrouting --f .{filepath} --conf ./osm2pgrouting-2.3.8/mapconfig_for_cars.xml --host PGROUTING --dbname postgres --schema public --username postgres --password cca_roteirizador --port 5432 --addnodes --attributes --tags --chunk 1000"
-                print(f"\Insert: {command}")
-                result = subprocess.run(command.split(' '), capture_output=True, text=True, check=True)
-                print(result.stdout)
-                print(result.stderr)
+        try: 
+            if os.path.exists(self.volume_path):
+                print(self.volume_path)
+                for filename in os.listdir(self.volume_path):
+                    filepath = os.path.join(self.volume_path, filename)
+                    command = f"osm2pgrouting --f .{filepath} --conf ./osm2pgrouting-2.3.8/mapconfig_for_cars.xml --host PGROUTING --dbname postgres --schema public --username postgres --password cca_roteirizador --port 5432 --addnodes --attributes --tags --chunk 1000"
+                    print(f"\Insert: {command}")
+                    result = subprocess.run(command.split(' '), capture_output=True, text=True, check=True)
+                    print(result.stdout)
+                    print(result.stderr)
+        except Exception as e:
+            print('Deu errroo', e)
