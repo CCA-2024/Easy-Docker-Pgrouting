e89fc27a3edcfee7bef4a9576de7339b3dc3a300
DevPyPedro <dev.santiago.felicio@gmail.com>
2025-01-15 17:28:02 -0300

    .

--- src/main_postgis.py
+++ src/main_postgis.py
@@ -1,14 +1,5 @@
-import time
-import os
-
 from consumer import OsmComsumer
 
 print('BOARA LAAAAAAAAAA')
 execute = OsmComsumer()
-
-while True:
-    if os.path.isfile(os.path.join('OSMs', 'ok.txt')):
-        execute.run_file()
-        break
-    else:
-        print('aguardando o arquivo')
+execute.run_file()
